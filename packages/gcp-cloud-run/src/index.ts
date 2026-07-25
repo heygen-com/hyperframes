4d165d79474049e85a43da011b753b75b20c3c75
James <james.russo@heygen.com>
2026-07-25 19:00:11 +0000

    fix(gcp-cloud-run): normalize v2 integrity codes

--- packages/gcp-cloud-run/src/index.ts
+++ packages/gcp-cloud-run/src/index.ts
@@ -21,24 +21,36 @@
 export { createApp, dispatch, type HandlerDeps, startServer, unwrapEvent } from "./server.js";
 export {
   type AssembleEvent,
+  type AssembleV1Event,
+  type AssembleV2Event,
   type AssembleResultBody,
   type CloudRunAction,
   type CloudRunEvent,
+  type CloudRunPlanProtocol,
   type CloudRunResult,
   type PlanEvent,
   type PlanResultBody,
+  type PlanV1Event,
+  type PlanV1ResultBody,
+  type PlanV2Event,
+  type PlanV2ResultBody,
   type RenderChunkEvent,
   type RenderChunkResultBody,
+  type RenderChunkV1Event,
+  type RenderChunkV2Event,
   type SerializableDistributedRenderConfig,
 } from "./events.js";
 export { ChromeBinaryUnavailableError, resolveChromeExecutablePath } from "./chromium.js";
 export {
   downloadGcsObjectToFile,
+  downloadGcsObjectToFileVerified,
   formatGcsUri,
   type GcsLocation,
   parseGcsUri,
+  sha256File,
   tarDirectory,
   untarDirectory,
+  uploadContentAddressedFileToGcs,
   uploadFileToGcs,
 } from "./gcsTransport.js";
 
