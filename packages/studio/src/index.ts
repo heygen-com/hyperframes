f364b44c2d4355ab7ca69035fc3755455174eb68
Miguel Angel Simon Sierra <miguel.sierra@heygen.com>
2026-09-18 12:25:28 -0400

    feat(studio): export Button, IconButton, Tabs and Tooltip from the package root

--- packages/studio/src/index.ts
+++ packages/studio/src/index.ts
@@ -1,3 +1,10 @@
+// UI primitives
+export { Button, buttonBase, buttonVariants } from "./components/ui/Button";
+export type { ButtonSize, ButtonVariant, PreviewState } from "./components/ui/Button";
+export { IconButton } from "./components/ui/IconButton";
+export { Tab, TabPanel, Tabs, TabsList } from "./components/ui/Tabs";
+export { Tooltip } from "./components/ui/Tooltip";
+
 // NLE Layout
 export { EditorShell } from "./components/EditorShell";
 export { NLEPreview } from "./components/nle/NLEPreview";
